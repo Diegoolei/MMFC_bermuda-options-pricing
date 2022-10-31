842853a9cf56e21f5e5849808d279f0daabc01c4
Diegoolei <Diegoolei@github.com>
2022-10-31 17:00:30 -0300

    Muchos argumentos heredados -> clase

--- bermudas.py
+++ bermudas.py
@@ -1,18 +1,28 @@
 from random import random
-from numpy import exp
+from numpy import exp, sqrt
 """
 Desarrollar un algoritmo que genere los valores de corte, 
 utilizando n trayectorias de un movimiento browniano geometrico, 
 para una opcion bermuda con madurez T, que puede ejercerse en t=T/3, 2T/3 o T
 """
-def cut_gen(s0, r, sigma, T, K, cantidad):
-    return movimiento_geometrico_browniano(s0, r, sigma, T, K, cantidad)
 
-def movimiento_browniano(media, sigma, cantidad):
-    return random.normal(media, sigma, cantidad)
+class Bermudas():
+    def __init__(self, s0, r, sigma, tiempo, K, cantidad):
+        self.s0 = s0
+        self.r = r
+        self.sigma = sigma
+        self.tiempo = tiempo
+        self.K = K
+        self.cantidad = cantidad
 
-def movimiento_geometrico_browniano(s0, tendencia, t, volatilidad, cantidad):
-    return s0 * exp(tendencia * t + volatilidad * movimiento_browniano(tendencia, volatilidad, cantidad))
+    def cut_gen(self):
+        return self.movimiento_geometrico_browniano()
+
+    def movimiento_browniano(self):
+        return random.normal(self.media * self.tiempo, self.sigma * sqrt(self.tiempo), self.cantidad)
+
+    def movimiento_geometrico_browniano(self):
+        return self.s0 * exp(self.tendencia * self.tiempo + self.volatilidad * self.movimiento_browniano())
 
 """
 Considerar: S0=36, r=0.06, σ=0.2, T= 1 año, K=35
@@ -24,4 +34,6 @@ K:
 {Tendencia?}
 """
 
-print(cut_gen(36, 0.06, 0.2, 1, 35))
+bermudas = Bermudas(36, 0.06, 0.2, 1, 35, 8)
+
+print(bermudas.cut_gen())
